986af7c10a77ac314af57ceb864adf8e4ff48271
BogdanNWJ <42960709+BogdanNWJ@users.noreply.github.com>
2020-06-02 13:57:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var calculateComplexity = function (password){
+    var complexity = 0;
+
+    var regExps = [
+        /[a-z]/,
+        /[A-Z]/,
+        /[0-9]/,
+        /.{8}/,
+        /.{16}/
+    ];
+
+    regExps.forEach(function (regexp){
+        if (regexp.test(password)){
+            complexity++;
+        }
+    });
+
+    return {
+        value:complexity,
+        max: regExps.length
+    };
+};
+
+var checkPasswdStr = function (password){
+    var progress = document.querySelector('#passwdc'),
+        complexity = calculateComplexity(this.value);
+
+    progress.value = complexity.value;
+    progress.max = complexity.max;
+};
+
+var input=document.querySelector('#passwdf');
+input.addEventListener('keyup',checkPasswdStr);
